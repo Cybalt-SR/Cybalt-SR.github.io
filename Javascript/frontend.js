6f725086be94922123660f9f55e1365addfbcbb6
Cybalt-SR <cybalt.sr@gmail.com>
2022-05-21 00:46:35 +0800

    js overhaul

---
+++ Javascript/frontend.js
@@ -0,0 +1,21 @@
+function togglesidebar() {
+    var navbar = document.getElementById('yuynLe');
+    var icon = document.getElementById('expandicon');
+
+    if (navbar.classList.contains("jsnVQ") == false) {
+        navbar.classList.add('jsnVQ');
+        icon.classList.add('o0iVae');
+    } else {
+        navbar.classList.remove('jsnVQ');
+        icon.classList.remove('o0iVae');
+    }
+}
+
+function updatefbembed(){
+    var embed = document.getElementById('fbembed');
+    var newsrc = 'https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2F306639766125525&width=';
+    newsrc += embed.offsetWidth + '';
+    newsrc += '&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=';
+    newsrc += embed.offsetHeight + '';
+    embed.src = newsrc;
+}
