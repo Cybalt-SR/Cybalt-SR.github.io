46c40b9096ce37f9d1eb9bc22789b0e84f0cec0c
Cybalt-SR <cybalt.sr@gmail.com>
2022-07-16 16:22:06 +0800

    Finalization

--- frontend.js
+++ frontend.js
@@ -55,60 +55,97 @@ function prepContent(data) {
   return data.contentWindow.document.body.innerHTML.substring(59).slice(0, -6);
 }
 
-//Youtube Embed and thumbnail Insertion
-var cid = 'UCMKaNzK1FpuLunon71O693w';
-var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");
-function loadVideo(iframe) {
-  $.getJSON(reqURL + cid,
-    function (data) {
-      var videoNumber = (iframe.getAttribute('vnum') ? Number(iframe.getAttribute('vnum')) : 0);
-      console.log(videoNumber);
-      var link = data.items[videoNumber].link;
-      id = link.substr(link.indexOf("=") + 1);
-      iframe.setAttribute("src", "https://youtube.com/embed/" + id + "?controls=0&autoplay=1");
-    }
-  );
-}
-function loadThumbnail(element) {
-  $.getJSON(reqURL + cid,
-    function (data) {
-      var videoNumber = (element.getAttribute('vnum') ? Number(element.getAttribute('vnum')) : 0);
-      console.log(videoNumber);
-      var link = data.items[videoNumber].link;
-      id = link.substr(link.indexOf("=") + 1);
-      element.style.backgroundImage = 'url("' + "http://img.youtube.com/vi/" + id + "/sddefault.jpg" + '")';
-
-      GetTitle(id, function (title) {
-        element.querySelector("span").innerHTML = title;
-      });
-    }
-  );
-}
-var iframes = document.getElementsByClassName('latestVideoEmbed');
-for (var i = 0, len = iframes.length; i < len; i++) {
-  loadVideo(iframes[i]);
-}
-var elements = document.getElementsByClassName('latestVideoThumbnail');
-for (var i = 0, len = elements.length; i < len; i++) {
-  loadThumbnail(elements[i]);
-}
-
-// Youtube API getters
+// ================= Youtube API ================= //
 var yt_api_key = GetAPIKEY();
 
-function GetTitle(yt_video_id, func) {
+//TITLE GETTER
+function GetTitle(yt_video_id, element, func) {
   var yt_snippet_endpoint = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + yt_video_id + "&key=" + yt_api_key;
 
-  var jqxhr = $.getJSON(yt_snippet_endpoint)
+  $.getJSON(yt_snippet_endpoint)
     .done(function (data) {
       var title = data.items[0].snippet.title + "";
-      func(title);
+      func(title, element);
       return;
     })
     .fail(function () {
       return "null title";
     });
 }
+
+// ====== YOUTUBE FEEDS RELIANT ====== //
+var cid = 'UCMKaNzK1FpuLunon71O693w';
+var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=") + cid;
+console.log(reqURL);
+
+//ID GETTER
+function GetVidNum(element) {
+  return (element.getAttribute('vnum') ? Number(element.getAttribute('vnum')) : 0);
+}
+//BG SETTER
+function SetBgThumbnail(element, id) {
+  element.style.backgroundImage = 'url("' + "http://img.youtube.com/vi/" + id + "/sddefault.jpg" + '")';
+}
+
+$.getJSON(reqURL, function (data) { InitFeedVideos(data); });
+
+function InitFeedVideos(data) {
+  var iframes = document.getElementsByClassName('feedVideoEmbed');
+  for (var i = 0, len = iframes.length; i < len; i++) {
+    var element = iframes[i];
+    var vdata = data.items[GetVidNum(element)];
+    var link = vdata.link;
+    var id = link.substr(link.indexOf("=") + 1);
+    element.setAttribute("src", "https://youtube.com/embed/" + id + "?controls=0&autoplay=1");
+  }
+
+  //THUMBNAIL SETTER FOR FEED VIDEOS
+  var elements = document.getElementsByClassName('feedVideoThumbnail');
+  for (var i = 0, len = elements.length; i < len; i++) {
+    var element = elements[i];
+    var vdata = data.items[GetVidNum(element)];
+    var link = vdata.link;
+    var id = link.substr(link.indexOf("=") + 1);
+
+    SetBgThumbnail(element, id);
+    element.querySelector("span").innerHTML = vdata.title;
+
+    element.setAttribute("tblink", "https://youtube.com/embed/" + id + "?controls=0&autoplay=1")
+  }
+}
+
+var yt_playlist_id = 'PLoS_G_Bd60ex0W6ihfqMbd6fa5i6mbdJS';
+var yt_playlist_endpoint = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=100&playlistId=" + yt_playlist_id + "&key=" + yt_api_key;
+console.log(yt_playlist_endpoint);
+$.getJSON(yt_playlist_endpoint, function (data) { InitPlaylistVideos(data); });
+
+//THUMBNAIL SETTER FOR PLAYLIST VIDEOS
+function InitPlaylistVideos(data) {
+  var elements = document.getElementsByClassName('playlistVideoThumbnail');
+  for (var i = 0, len = elements.length; i < len; i++) {
+    var element = elements[i];
+    var vnum = GetVidNum(element);
+    var itemindex = data.items.length - vnum - 1;
+
+    if (data.items[itemindex] != null) {
+      id = data.items[itemindex].snippet.resourceId.videoId;
+      SetBgThumbnail(element, id);
+
+      GetTitle(id, element, function (title, in_elem) {
+        in_elem.querySelector("span").innerHTML = title;
+      });
+
+      element.setAttribute("tblink", "https://youtube.com/embed/" + id + "?controls=0&autoplay=1")
+    }
+  }
+}
+
+// ====== YOUTUBE PLAYLIST RELIANT ====== //
+
+//get playlist items link:
+//https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=9&playlistId=PLL1ekZ31_Q4Lhjvq1F30ModgubXGMrL61&key=AIzaSyBT54ZwWyTt-U23kAEBKYptpKjIoAIhZYs
+
+// ================= +++++++++++++++ ================= //
 
 //footer insertion
 
